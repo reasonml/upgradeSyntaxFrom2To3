c0dc4a7435e4152e785fb9e90cb76245f1e20de1
Cheng Lou <chenglou92@gmail.com>
2017-11-01 13:04:37 -0700

    Cleaner output, lockfile check, bsconfig check + eager bailing

--- upgradeSyntaxFrom2To3.js
+++ upgradeSyntaxFrom2To3.js
@@ -9,19 +9,44 @@ if (process.argv.length <= 2) {
   console.error('You need to pass a list of the files you\'d like to upgrade, like so: `upgradeSyntaxFrom2To3 src/*`');
   process.exit(1);
 }
-const filesToMigrate = process.argv.slice(2);
-console.log(`\u{1b}[36mStarting Reason syntax 2 -> 3 migration!\u{1b}[39m Note that this is just a dumb script that takes all your files and do:
 
-  ./node_modules/bs-platform/bin/refmt.exe --print binary_reason foo.re | ./node_modules/bs-platform/bin/refmt2.exe --parse binary_reason --print re`)
+const cyan = '\u{1b}[36m';
+const cyanEnd = '\u{1b}[39m';
+const dim = '\u{1b}[2m';
+const dimEnd = '\u{1b}[22m';
+
+const filesToMigrate = process.argv.slice(2);
+console.log(`${cyan}Starting Reason syntax 2 -> 3 migration!${cyanEnd}
+${dim}Note that this is just a dumb script that takes all your files and do:
+
+  ./node_modules/bs-platform/bin/refmt.exe --print binary_reason foo.re | ./node_modules/bs-platform/bin/refmt2.exe --parse binary_reason --print re${dimEnd}
+`)
 
 let filesThatAreBackups = [];
-let filesThatAreNotReason = [];
 let filesThatAreInvalid = [];
 let filesThatHaveBackups = [];
 const binariesRoot = path.join('.', 'node_modules', 'bs-platform', 'bin');
 const bsconfig = path.join('.', 'bsconfig.json');
 
+const packageLock = path.join('.', 'package-lock.json');
+const shrinkwrap = path.join('.', 'npm-shrinkwrap.json');
+const yarnlock = path.join('.', 'yarn.lock');
+
 function migrate(files) {
+  let addRefmt3ToBsconfig;
+
+  // check that bsconfig exists
+  if (fs.existsSync(bsconfig)) {
+    const bsconfigContent = fs.readFileSync(bsconfig, {encoding: 'utf-8'});
+    addRefmt3ToBsconfig = bsconfigContent.indexOf('refmt') === -1
+      ? `${cyan}Please add \`"refmt": 3\` to your \`bsconfig.json\`${cyanEnd}`
+      : `Add \`"refmt": 3\` to your \`bsconfig.json\`, then run the build again to verify that nothing errored.`;
+  } else {
+    console.log(`${cyan}
+We can't found a bsconfig.json file in this project${cyanEnd}; did you invoke the upgrade command at the right place?`);
+    return;
+  }
+
   const oldBinary = path.join(binariesRoot, 'refmt.exe');
   const newBinary = path.join(binariesRoot, 'refmt3.exe');
 
@@ -34,7 +59,7 @@ function migrate(files) {
     }
 
     if (extension !== '.re' && extension !== '.rei') {
-      filesThatAreNotReason.push(file);
+      // not reason file
       return;
     }
 
@@ -79,43 +104,44 @@ function migrate(files) {
   })
 
   if (filesThatAreBackups.length !== 0) {
-      console.log(`\u{1b}[36m
+      console.log(`${cyan}
 The follow files are skipped, because they're backup files from a previous run of this script:
-\u{1b}[39m`);
+${cyanEnd}`);
       console.log(filesThatAreBackups.map(f => '- ' + f).join('\n'));
-  }
-  if (filesThatAreNotReason.length !== 0) {
-      console.log(`\u{1b}[36m
-The follow files are skipped, because they're not Reason files:
-\u{1b}[39m`);
-      console.log(filesThatAreNotReason.map(f => '- ' + f).join('\n'));
   }
 
   if (filesThatAreInvalid.length !== 0) {
-      console.log(`\u{1b}[36m
-The follow files are skipped, because they contain syntax errors:
-\u{1b}[39m`);
+      console.log(`${cyan}
+The follow files failed to transform (syntax error or missing refmt):
+${cyanEnd}`);
       console.log(filesThatAreInvalid.map(f => '- ' + f).join('\n'));
   }
 
   if (filesThatHaveBackups.length !== 0) {
-      console.log(`\u{1b}[36m
+      console.log(`${cyan}
 The follow files are skipped, because they already have their backup (and are this presumed to be transformed already):
-\u{1b}[39m`);
+${cyanEnd}`);
       console.log(filesThatHaveBackups.map(f => '- ' + f).join('\n'));
   }
 
-  const fileContent = fs.readFileSync(bsconfig, {encoding: 'utf-8'});
-  const addRefmt3ToBsconfig = (fileContent.indexOf('refmt') === -1)
-    ? `\u{1b}[36mPlease add \`"refmt": 3\` to your \`bsconfig.json\`\u{1b}[39m`
-    : `Add \`"refmt": 3\` to your \`bsconfig.json\`, then run the build again to verify that nothing errored.`;
+  const packageLockContent = fs.existsSync(packageLock) ? fs.readFileSync(packageLock, {encoding: 'utf-8'}) : '';
+  const shrinkwrapContent = fs.existsSync(shrinkwrap) ? fs.readFileSync(shrinkwrap, {encoding: 'utf-8'}) : '';
+  const yarnlockContent = fs.existsSync(yarnlock) ? fs.readFileSync(yarnlock, {encoding: 'utf-8'}) : '';
 
-  console.log(`\u{1b}[36m
-All done! IMPORTANT things to do after the codemod:\u{1b}[39m
+  const hasLockedBsPlatform =
+    packageLockContent.indexOf('bs-platform') >= 0
+    || shrinkwrapContent.indexOf('bs-platform') >= 0
+    || yarnlockContent.indexOf('bs-platform') >= 0;
+
+  const unlockBsPlatform = hasLockedBsPlatform
+    ? `\n- ${cyan}You seem to have locked bs-platform in your npm/yarn lockfile${cyanEnd}. Make sure you've actually upgraded bs-platform!`
+    : '';
+
+  console.log(`${cyan}
+All done! IMPORTANT things to do after the codemod:${cyanEnd}
 
 - We've backed up your old code at \`yourFileName.backup\`. Feel free to examine & delete them.
-- ${addRefmt3ToBsconfig}
-- If you've checked in the JS artifacts, make sure they didn't change. Your artifacts are your snapshot tests =)
+- ${addRefmt3ToBsconfig}${unlockBsPlatform}
 
 If there's any change to your build or artifacts, please check and file us an issue. Apologies in advance; it's a big refactoring.
 
